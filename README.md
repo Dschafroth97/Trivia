c09d8e1e97fda2b88db4270eb8ee5b296e18e61e
Donovan Schafroth <donovanschafroth@gmail.com>
2019-03-25 18:33:53 -0500

    html layed out, css mostly

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Trivia
+// Instructions for HW
+
+* You'll create a trivia form with multiple choice or true/false options (your choice).
+
+* The player will have a limited amount of time to finish the quiz. 
+
+  * The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+
+* Don't let the player pick more than one answer per question.
+
+* Don't forget to include a countdown timer.
+
+// Psuedocode
+
+// Layout HTML and CSS, create empty <div> for quiz to be inserted
+// Start game screen and button
+
---
+++ assets/javascript/logic.js
@@ -0,0 +1,43 @@
+// GLOBAL VARIABLES
+// ==============================================================
+var quizDisplay = $("#quiz-area");
+var question = [
+
+];
+var timer = 5;
+// * You'll create a trivia form with multiple choice or true/false options (your choice).
+// * The player will have a limited amount of time to finish the quiz. 
+// * The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+// * Don't let the player pick more than one answer per question.
+// * Don't forget to include a countdown timer.
+
+
+
+
+
+
+// FUNCTIONS
+// ==============================================================
+function startGame(){
+
+    $("#start").on("click", function(){
+
+        $(this).hide();
+    });
+
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+// MAIN PROCESS
+// ==============================================================
+startGame();
